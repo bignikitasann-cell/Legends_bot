8ae2cb4de483163ea4040bb52da257b5ba3f08ab
bignikitasann-cell <bignikitasann@gmail.com>
2026-05-20 16:49:49 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import os
+ import os
 import logging
 from dotenv import load_dotenv
 from telegram import Update
@@ -9,11 +9,15 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 logging.basicConfig(level=logging.INFO)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ Бот работает!")
+    await update.message.reply_text("✅ Бот работает на Render через Docker!")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f"Ты написал: {update.message.text}")
 
 def main():
     app = Application.builder().token(TELEGRAM_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     port = int(os.environ.get("PORT", 8080))
     app.run_webhook(listen="0.0.0.0", port=port, webhook_url=f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME')}/{TELEGRAM_TOKEN}")
 
